f2e49351f0ecfd20cf21c6d8bf494b5e45f2dc74
ChandranSukirthan <sukirsukirthan21@gmail.com>
2026-05-07 22:54:24 +0530

    last changable

--- duinophile/src/main/java/com/duinophile/repository/LessonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.duinophile.repository;
-
-import com.duinophile.model.Lesson;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface LessonRepository extends MongoRepository<Lesson, String> {
-    List<Lesson> findByCourseId(String courseId);
-}
